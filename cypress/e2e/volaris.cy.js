703cfff41c010de4a5bed3bd67a77088f0b8958c
maica-angela <accia.alena21@gmail.com>
2023-08-10 16:31:08 +0800

    update second page

--- cypress/e2e/volaris.cy.js
+++ cypress/e2e/volaris.cy.js
@@ -22,15 +22,27 @@ describe("Testing", () => {
         cy.get('[class="btn btn-large col-12 search-btn mat-flat-button mat-button-base mat-primary"]')
             .scrollIntoView().click({ force: true });
 
-
         cy.wait(5000);
 
-        cy.get('[class="flightLists ng-star-inserted"]')
-            .parent()
-            .find('[div:first-child]')
-            .click();
-        cy.get('[class="btn btn-small d-none d-md-block mat-raised-button mat-button-base mat-terceary ng-star-inserted"]')
-            .click();
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        })
+
+        cy.get('[id="Flightlists"]').contains('keyboard_arrow_down').eq(0).click();
+        cy.contains('Select').eq(0).click();
+
+        cy.get('[id="Flightlists"]').contains('keyboard_arrow_down').eq(0).click();
+        cy.get('[class="benefitsList ng-star-inserted uplift"]').eq(0).click({ force: true });
+
+        cy.get('[aria-label="Close"]').click({ force: true });
+
+        cy.get('[class="check"]').click({ force: true });
+
+        cy.get('[aria-label="Continue To Seat"]').click({ force: true });
+
+
     })
 
     class testData {
