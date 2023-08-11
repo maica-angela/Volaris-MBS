5f4470805a8d95f9778dbb6bdc478b2e38730264
maica-angela <accia.alena21@gmail.com>
2023-08-11 14:20:12 +0800

    update and get pnr

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,22 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("task", {
+        setData: newData => {
+            data = newData !== null ? { ...data, ...newData } : {};
+            return data;
+        },
+        getData: () => {
+            return data;
+        },
+        cleanData: () => {
+            return {};
+        },
+    })
+
     },
     defaultCommandTimeout: 25000,
+
+    
   },
 };
--- cypress/e2e/volaris.cy.js
+++ cypress/e2e/volaris.cy.js
@@ -3,7 +3,7 @@
 describe("Testing", () => {
 
 
-    it("Domestic RT 1 PAX Zero Fare before 72 hours", () => {
+    it("Domestic RT 1 PAX(Mex) Zero Fare before 72 hours", () => {
         const testdataa = new testData();
 
 
@@ -19,7 +19,6 @@ describe("Testing", () => {
             .click();
         cy.get('[class="btn btn-large col-12 search-btn mat-flat-button mat-button-base mat-primary"]')
             .scrollIntoView().click({ force: true });
-
 
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from
@@ -50,35 +49,56 @@ describe("Testing", () => {
             .click();
         cy.get('[formcontrolname="dobYear"]').click({ force: true });
         cy.contains(' 2000 ').click({ force: true });
-
-
         cy.get('[class="mat-autocomplete-trigger mat-input-element mat-form-field-autofill-control cdk-text-field-autofill-monitored ng-untouched ng-pristine ng-invalid"]')
             .eq(0).click();
-        cy.contains('[class="mat-autocomplete-panel hybridselect mat-autocomplete-visible ng-star-inserted"]')
-            .click();
-
+        cy.get('[class="mat-autocomplete-panel hybridselect mat-autocomplete-visible ng-star-inserted"]')
+            .contains(' Mexico ').click();
         cy.get('[class="btn continue mat-flat-button mat-button-base btn-large mat-primary"]')
             .click({ force: true });
 
-        cy.get('[formcontrolname="email"]').type('fairytale01@yopmail.com', { force: true });
-        cy.get('[formcontrolname="verifyEmail"]').type('fairytale01@yopmail.com', { force: true });
 
+        cy.get('[class="mat-checkbox-label"]') //[formcontrolname = "usePrimaryCustomerName"]
+            .eq(0).click({ force: true });
+        cy.get('[formcontrolname="email"]').type('fairytale001@yopmail.com', { force: true });
+        cy.get('[formcontrolname="verifyEmail"]').type('fairytale001@yopmail.com', { force: true });
         cy.get('[formcontrolname="phoneNumber"]').type('fairytale01@yopmail.com');
-
         cy.get('[id="mat-checkbox-10-input"]').click({ force: true });
-        cy.get('button[class="btn mat-flat-button mat-button-base btn-large mat-primary"]')
+        cy.get('[class="mat-checkbox-label"]')//formcontrolname="privacyNotice"
+            .eq(1).click({ force: true });
+        cy.get('[class="btn mat-flat-button mat-button-base btn-large mat-primary"]')
             .click({ force: true });
-
+        cy.wait(10000);
 
         cy.get('[formcontrolname="cardNumber"]').type('5555444433331111');
-
-        cy.get('[formcontrolname="cardExpMonth"]').eq(9).click({ force: true });
-        cy.get('[formcontrolname="cardExpYear"]').eq(7).click({ force: true });
-        cy.get('[formcontrolname="cardCVV"]').type(737);
-
-        cy.get('[id="mat-checkbox-13-input"]').type('fairytale01@yopmail.com');
-        cy.get('button[class="btn btn-large pay-my-trip mat-flat-button mat-button-base mat-primary"]')
+        cy.get('[formcontrolname="cardExpMonth"]').click();
+        cy.get('[class="mat-autocomplete-panel hybridselect mat-autocomplete-visible ng-star-inserted"]')
+            .contains('10')
             .click({ force: true });
+        cy.get('[formcontrolname="cardExpYear"]').click();
+        cy.get('[class="mat-autocomplete-panel hybridselect mat-autocomplete-visible ng-star-inserted"]')
+            .contains('2025')
+            .click({ force: true });
+        cy.get('[formcontrolname="cardCVV"]')
+            .type('737');
+
+        cy.get('[class="mat-checkbox-label"]')
+            .eq(4).click();
+        cy.get('[formcontrolname="street"]').type('street 123');
+        cy.get('[formcontrolname="city"]').type('city');
+        cy.get('[formcontrolname="zipCode"]').type('1234');
+        cy.get('[class="mat-checkbox-label"]')
+            .eq(7).click();
+
+        cy.get('[class="btn btn-large pay-my-trip mat-flat-button mat-button-base mat-primary"]')
+            .click();
+
+        cy.wait(10000);
+        cy.get('[class="code"]').invoke("text").then(text => {
+            cy.task("setData", { text });
+        });
+        cy.task("getData").then(({text})=>{
+            cy.get("input").type(text);
+        });
     })
 
     class testData {
